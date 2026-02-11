7b7c16cc53cf412ecf1d3572c9c0833ca86bceae
Gabriel Pereira Nunes <gp9075254@gmail.com>
2026-02-10 21:40:04 -0300

    Semana 6 Dia 3: Conceitos de DAG e orquestração

---
+++ 05_sql_fundamentals/exercises/semana_3_dia_3.sql
@@ -0,0 +1,82 @@
+--Liste todos os países com sua população em 2020
+
+
+--Exercício 2
+
+--Liste todos os países, mesmo os que não têm dados populacionais
+
+
+--Exercício 3
+
+--Países que NÃO possuem registro de população
+
+
+
+--Exercício 4
+
+--População total por continente em 2020
+
+
+
+--Exercício 5
+
+--Top 10 países mais populosos em 2020
+
+
+
+--Exercício 6
+
+--Quantidade de países por continente
+
+
+
+--Exercício 7
+
+--População média dos países por continente em 2020
+
+
+--Exercício 8
+
+--Países com população maior que a média do seu continente (2020)
+
+
+
+
+Exercício 9
+
+
+
+Exercício 10
+
+Países que NÃO possuem cidades cadastradas
+
+
+
+Exercício 11
+
+População total das cidades vs população do país (2020)
+
+
+
+Exercício 12
+
+Países onde a soma da população das cidades é menor que a população do país
+
+
+
+Exercício 13
+
+Cidades mais populosas do mundo com país e continente
+
+
+
+Exercício 14
+
+Crescimento populacional por país (2020 vs 2010)
+
+
+
+Exercício 15
+
+Top 5 países com maior crescimento populacional
+
